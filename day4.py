98653804e261a594808421a4ea27eb23e4c43c7d
jeff-PC <jefferyfrederic@gmail.com>
2022-12-05 21:41:10 -0700

    day 4 but better

--- day4.py
+++ day4.py
@@ -7,12 +7,12 @@ with open('day4.in') as file:
 pairs = 0
 for entry in data:
     first, second = entry.split(",")
-    first = first.split("-")
-    second = second.split("-")
+    first = [int(i) for i in first.split("-")]
+    second = [int(i) for i in second.split("-")]
 
-    if int(first[0]) <= int(second[0]) and int(first[1]) >= int(second[1]):
+    if first[0] <= second[0] and first[1] >= second[1]:
         pairs += 1
-    elif int(second[0]) <= int(first[0]) and int(second[1]) >= int(first[1]):
+    elif second[0] <= first[0] and second[1] >= first[1]:
         pairs += 1
 
 print("Answer to part 1:", pairs)
